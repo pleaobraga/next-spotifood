001f65d6fa6e1fe2a892f2c2d8c341a30e1123b1
Pedro Braga <pleao.braga@gmail.com>
2024-03-07 18:04:03 -0300

    feat: update Circular Loader interface

--- src/components/atom/FilterButton/FilterButton.stories.tsx
+++ src/components/atom/FilterButton/FilterButton.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { FilterButton } from '.'
 
 const meta = {
-  title: 'Components/Atom/FilterButton',
+  title: 'Components/ Atom/FilterButton',
   component: FilterButton,
   parameters: {
     layout: 'centered',
