7fd091e1f0478644f800572547f173b4f0368df6
Pedro Braga <pleao.braga@gmail.com>
2024-03-08 13:16:28 -0300

    feat: improve card text behavior

--- src/components/Molecule/Card/Card.tsx
+++ src/components/Molecule/Card/Card.tsx
@@ -10,7 +10,7 @@ function Card({ imgSrc, link, name }: CardProps) {
   return (
     <div className="mx-[0.5rem] mt-[1rem] flex w-[230px] justify-center rounded bg-gray-400 p-[16px] shadow hover:bg-gray-200">
       <a
-        className="flex flex-col no-underline"
+        className="flex w-full flex-col no-underline"
         href={link}
         target="_blank"
         rel="noreferrer"
@@ -18,7 +18,7 @@ function Card({ imgSrc, link, name }: CardProps) {
         <div className="h-[200px] w-[200px]">
           <Image alt="albun image" imgSrc={imgSrc} />
         </div>
-        <h3 className="mt-[20px] truncate whitespace-nowrap font-bold text-white">
+        <h3 className="mt-[20px] w-full truncate whitespace-nowrap text-nowrap font-bold text-white">
           {name}
         </h3>
       </a>
