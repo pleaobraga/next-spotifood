5f0eabd8b941e9009ff083810d55bef7ec6393e6
Pedro Braga <pleao.braga@gmail.com>
2024-03-08 13:53:22 -0300

    feat: improve textformfield UI

--- src/components/Molecule/TextFormField/TextFormFieldinput.tsx
+++ src/components/Molecule/TextFormField/TextFormFieldinput.tsx
@@ -16,7 +16,7 @@ export function TextFormFieldInput({
   return (
     <Control asChild>
       <input
-        className="w-full rounded border border-solid border-gray-100 bg-gray-400 px-[14px] py-[18.5px] text-white placeholder-gray-100"
+        className="w-full rounded border border-solid border-gray-100 bg-gray-400 px-[14px] py-[18.5px] text-white placeholder-gray-100 hover:border-white focus:border-green"
         name={name}
         value={value}
         type={type}
