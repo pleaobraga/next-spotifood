d8f7df34e3592c4b8ad64ef7061a56db03d86497
Pedro Braga <pleao.braga@gmail.com>
2024-03-09 09:59:24 -0300

    feat: improve cardlist UI

--- src/components/Organism/CardList/CardList.tsx
+++ src/components/Organism/CardList/CardList.tsx
@@ -19,17 +19,20 @@ function CardList({ cards }: CardListProps) {
   }
 
   return (
-    <div className="flex flex-wrap gap-[1rem]">
-      {cards.map(({ external_urls, id, name, images }) => {
-        return (
-          <Card
-            key={id}
-            name={name}
-            imgSrc={images[0].url}
-            link={external_urls.spotify}
-          />
-        )
-      })}
+    <div>
+      <h2 className="text-3xl font-bold text-white">Playlists</h2>
+      <div className="mt-[2rem] flex flex-wrap gap-[1rem]">
+        {cards.map(({ external_urls, id, name, images }) => {
+          return (
+            <Card
+              key={id}
+              name={name}
+              imgSrc={images[0].url}
+              link={external_urls.spotify}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
