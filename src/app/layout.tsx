942b40bacfad6c27fe8e723639b111cde1c8ef34
Pedro Braga <pleao.braga@gmail.com>
2024-03-09 15:38:57 -0300

    feat: update project name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Next Spotifood',
+  title: 'Nextfy',
   description: 'Sptify like application using Next',
 }
 
--- src/components/Organism/Filter/Filter.tsx
+++ src/components/Organism/Filter/Filter.tsx
@@ -8,7 +8,7 @@ function Filter() {
   return (
     <header className="sticky top-0 z-10 w-full  bg-gray-800 shadow-[0_4px_3px_-0px_rgba(0,0,0,0.2)]">
       <div className="container mx-auto flex items-center gap-[3rem] p-6">
-        <h1 className="text-[2rem] font-bold text-green">Spotifood</h1>
+        <h1 className="text-[2rem] font-bold text-green">Nextfy</h1>
         <Form className="w-4/5">
           <TextFormField.Root name="search">
             <TextFormField.InputIcon
